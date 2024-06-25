f3381e3544feb276726f44c078d779691d3eccec
emnificent <49172724+emnificent@users.noreply.github.com>
2024-06-26 00:24:06 +0200

    feat: add search results page and legal page

---
+++ src/routes/legal/+page.svelte
@@ -0,0 +1,40 @@
+<main class="legal">
+  <h1>Mentions légales</h1>
+  <p>Le site LAEC.fr est un service de communication au public en ligne qui est édité à titre non professionnel (au sens de l'article 6, III-2 de la Loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique). En accord avec les dispositions de l'article sus-cité, l'auteur de ces lignes a donc choisi de conserver l'anonymat. Ces informations ont toutefois été transmises à l'hébergeur de ce site qui saura les délivrer à qui de droit.</p>
+  <h2>Hébergeur</h2>
+  <p>Le prestataire technique qui assure l'hébergement de LAEC.FR est la société OVH, dont le siège social se situe à Roubaix.<br />Société OVH, 2 rue Kellermann 59100 Roubaix<br />Téléphone : +33 8 99 70 17 61</p>
+  <h2>Politique de confidentialité</h2>
+  <h3>Statistiques et mesures d'audience</h3>
+  <p>Nous n'utilisons pas d'outils de mesure d'audience.<br />Les logs de connexion du serveur sont conservés pour une durée de 14 jours conformément aux dispositions européennes en matière de rétention de données.</p>
+  <h3>Accès aux données</h3>
+  <p>Conformément à la loi Informatique et Libertés 78-17 du 6 janvier 1978 modifiée ainsi qu'au Règlement européen sur la protection des données (RGPD), vous bénéficiez d'un droit d'accès, de rectification, d'opposition et d'effacement de vos données personnelles ainsi que d'un droit à la limitation du traitement vous concernant.<br/><br/>Pour exercer ces droits ou pour toute question sur le traitement de vos données dans ce dispositif, vous pouvez contacter par courrier électronique l'adresse suivante : actions.insoumises@gmail.com.<br/><br/>Si vous estimez, après nous avoir contactés, que les droits dont vous disposez ne sont pas respectés, vous pouvez adresser une réclamation à la CNIL.</p>
+  <h3>Litiges</h3>
+  <p>La loi applicable au présent contrat est la loi française. Dans toute hypothèse où un choix de loi serait possible, les parties conviennent que la loi applicable est la loi française.</p>
+</main>
+
+<!-- ne copie pas le CSS depuis ici, c'est du SCSS, ça permet d'aller plus vite, mais il doit être compiler pour fonctionner correctement, tu trouveras le CSS compilé dans /styles/compiled.css -->
+<style lang="scss">
+  .legal {
+    max-width: var(--bp-desktop--large);
+    margin: auto;
+    margin-block: 5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2.5rem;
+
+    & h1 {
+      font-size: var(--fs-title--large);
+    }
+
+    & h2, h3 {
+      text-transform: initial;
+      font-weight: var(--fw--bold);
+    }
+
+    & h3 {
+      font-size: var(--fs-text);
+      font-family: 'Barlow', 'sans-serif';
+      line-height: 1;
+    }
+  }
+</style>
---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,165 @@
+<div class="search-page__background">
+  <main class="search-page">
+    <div class="search-page__header">
+      <div class="search-page__input">
+        <input type="search" name="" id="" placeholder="Cherchez par mots clés: économie, justice, écologie...">
+        <button aria-label="lancer la recherche"><i class="fa-solid fa-magnifying-glass"></i></button>
+      </div>
+      <p>38 résultats pour "planification écologique"</p>
+    </div>
+
+    <div class="search-page__results">
+      <article>
+        <h2>Entamer la <mark>planification écologique</mark></h2>
+        <p>Abolir la taxe Macron de 10% sur les factures d’énergie, annuler la hausse programmée du prix du gaz au 1er juillet, plafonner les frais bancaires, faire la gratuité des premiers KwH, abolir les coupures d’électricité, de chaleur et de gaz (hors trêve hivernale), annuler les réformes Macron sur le revenu de solidarité active (RSA)</p>
+        <a href="/step">Lire la suite<span class="arrow"></span></a>
+      </article>
+      <article>
+        <h2>Entamer la <mark>planification écologique</mark></h2>
+        <p>Abolir la taxe Macron de 10% sur les factures d’énergie, annuler la hausse programmée du prix du gaz au 1er juillet, plafonner les frais bancaires, faire la gratuité des premiers KwH, abolir les coupures d’électricité, de chaleur et de gaz (hors trêve hivernale), annuler les réformes Macron sur le revenu de solidarité active (RSA)</p>
+        <a href="/step">Lire la suite<span class="arrow"></span></a>
+      </article>
+      <article>
+        <h2>Entamer la <mark>planification écologique</mark></h2>
+        <p>Abolir la taxe Macron de 10% sur les factures d’énergie, annuler la hausse programmée du prix du gaz au 1er juillet, plafonner les frais bancaires, faire la gratuité des premiers KwH, abolir les coupures d’électricité, de chaleur et de gaz (hors trêve hivernale), annuler les réformes Macron sur le revenu de solidarité active (RSA)</p>
+        <a href="/step">Lire la suite<span class="arrow"></span></a>
+      </article>
+      <article>
+        <h2>Entamer la <mark>planification écologique</mark></h2>
+        <p>Abolir la taxe Macron de 10% sur les factures d’énergie, annuler la hausse programmée du prix du gaz au 1er juillet, plafonner les frais bancaires, faire la gratuité des premiers KwH, abolir les coupures d’électricité, de chaleur et de gaz (hors trêve hivernale), annuler les réformes Macron sur le revenu de solidarité active (RSA)</p>
+        <a href="/step">Lire la suite<span class="arrow"></span></a>
+      </article>
+    </div>
+  </main>
+</div>
+
+<!-- ne copie pas le CSS depuis ici, c'est du SCSS, ça permet d'aller plus vite, mais il doit être compiler pour fonctionner correctement, tu trouveras le CSS compilé dans /styles/compiled.css -->
+<style lang="scss">
+  .search-page__background {
+    background-color: var(--c-green);
+    min-height: calc(100vh - 8.5rem);
+    display: flex;
+  }
+
+  .search-page {
+    width: 100%;
+    max-width: var(--bp-desktop--large);
+    margin: auto;
+    margin-block: 5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2.5rem;
+  }
+
+  .search-page__header {
+    width: 100%;
+    max-width: var(--bp-tablet);
+
+    & p {
+      text-align: center;
+      margin-top: 1rem;
+      color: var(--c-background);
+    }
+  }
+
+  .search-page__input {
+    display: flex;
+    gap: 1rem;
+    background-color: var(--c-background);
+    border-radius: 1rem;
+    overflow: hidden;
+    padding: 1rem 2rem;
+    border: 1px solid var(--c-text);
+
+    & input {
+      background-color: transparent;
+      color: var(--c-text);
+      width: 100%;
+      border: none;
+      outline: none;
+    }
+
+    & button {
+      width: 2rem;
+      height: 2rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: transparent;
+      background-color: transparent;
+      font-size: var(--fs-text--large);
+      opacity: 0.5;
+      transition: opacity 0.3s;
+
+      &:hover,
+      &:focus-visible {
+        opacity: 1;
+      }
+    }
+  }
+
+  .search-page__results {
+    display: flex;
+    flex-direction: column;
+    gap: 2.5rem;
+
+    & article {
+      background-color: var(--c-background);
+      border: 1px solid var(--c-text);
+      padding: 2.5rem;
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+
+      & h2 {
+        text-transform: initial;
+        font-weight: var(--fw--bold);
+      }
+
+      & a {
+        text-decoration: none;
+        color: var(--c-text);
+        font-weight: var(--fw--bold);
+        display: flex;
+        align-items: center;
+        gap: 0.35rem;
+        width: fit-content;
+        border-bottom: 1px solid transparent;
+        transition: border 0.3s;
+        
+        &:hover,
+        &:focus-visible {
+          border-bottom: 1px solid var(--c-text);
+        }
+      }
+
+      & .arrow {
+        background-color: var(--c-text);
+        width: 1.5rem;
+        height: 2px;
+        position: relative;
+
+        &::before,
+        &::after {
+          content: '';
+          position: absolute;
+          background-color: var(--c-text);
+          width: 0.75rem;
+          height: 2px;
+          right: -3.5px;
+        }
+      
+        &::before {
+          rotate: 45deg;
+          top: -3.5px;
+        }
+
+        &::after {
+          rotate: -45deg;
+          bottom: -3.5px;
+        }
+      }
+    }
+  }
+</style>
